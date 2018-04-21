ade850a7abfc1b454ba9fe4d4eee81f75ff2b9be
Prime <testagentprime@gmail.com>
2018-04-21 17:19:26 -0300

    ChromeDriver was added

---
+++ CITSmart/CITSmart/TestBase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace CITSmart
+{
+    [CodedUITest]
+    public class TestBase
+    {
+        #region Atributos de teste adicionais
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            
+        }
+
+        #endregion
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return _testContextInstance;
+            }
+            set
+            {
+                _testContextInstance = value;
+            }
+        }
+        private TestContext _testContextInstance;
+    }
+}
