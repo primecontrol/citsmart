5d724b53ef894ad021e616d3619a6edd1f67b68b
Prime <testagentprime@gmail.com>
2018-04-21 17:47:30 -0300

    References of Selenium and ExtentReports was added

--- CITSmart/CITSmart/TestBase.cs
+++ CITSmart/CITSmart/TestBase.cs
@@ -1,18 +1,28 @@
-﻿using Microsoft.VisualStudio.TestTools.UITesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UITesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 
 namespace CITSmart
 {
     [CodedUITest]
-    public class TestBase
+    public abstract class TestBase
     {
+        #region Globals
+
+        IWebDriver _driver = new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory);
+
+        #endregion
+
+
         #region Atributos de teste adicionais
 
         [TestInitialize()]
         public void MyTestInitialize()
         {
-            
+            _driver.Navigate().GoToUrl(new Uri("https://google.com"));
         }
 
         [TestCleanup()]
@@ -34,6 +44,10 @@ namespace CITSmart
                 _testContextInstance = value;
             }
         }
+
+        public IWebDriver Driver { get => Driver1; set => Driver1 = value; }
+        public IWebDriver Driver1 { get => _driver; set => _driver = value; }
+
         private TestContext _testContextInstance;
     }
 }
---
+++ CITSmart/CITSmart/Tests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CITSmart
+{
+    [CodedUITest]
+    [DeploymentItem(@"CITSmart\Drivers")]
+    public class Tests : TestBase
+    {
+        [TestMethod]
+        public void Test1()
+        {
+
+        }
+    }
+}
